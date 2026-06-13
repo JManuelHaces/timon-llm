a8be995c3c631f901bd157597d9a7c3123921240
JManuelHaces <jmanuelhaces@gmail.com>
2026-06-13 11:52:34 -0600

    feat(engine): perillas KnobVector + mapeo a vector de steering

---
+++ engine/timon_engine/__init__.py
@@ -0,0 +1,8 @@
+"""Timón engine: interfaz de steering y motores (mock + Gemma)."""
+
+from timon_engine.knobs import KnobVector, knobs_to_steering_vector
+
+__all__ = [
+    "KnobVector",
+    "knobs_to_steering_vector",
+]
